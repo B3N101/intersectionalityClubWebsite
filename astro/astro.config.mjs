82f02ddaac3c48b31951277c44f1d42d0236343d
Benjamin Feuer <64800026+B3N101@users.noreply.github.com>
2023-04-22 21:20:26 -0400

    update fonts + change base

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,21 +1,18 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
-
 import react from "@astrojs/react";
 import sanity from 'astro-sanity';
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-	site: "https://astro-sanity-minimal-starter.netlify.app/",
-	// Your public domain, e.g.: https://my-site.dev/
-	integrations: [
-		sitemap(),
-    react(),
-		sanity({
-			projectId: "e5w2c1ol",
-			dataset: "production",
-			apiVersion: "v2021-03-25",
-			useCdn: true,
-		}),
-	],
+  site: "https://astro-sanity-minimal-starter.netlify.app/",
+  // Your public domain, e.g.: https://my-site.dev/
+  integrations: [sitemap(), react(), sanity({
+    projectId: "e5w2c1ol",
+    dataset: "production",
+    apiVersion: "v2021-03-25",
+    useCdn: true
+  }), tailwind()]
 });
--- astro/src/components/ui/Navbar.tsx
+++ astro/src/components/ui/Navbar.tsx
@@ -1,7 +1,31 @@
+import "@fontsource/courgette";
+
 export default function Navbar() {
-  return (
-    <nav>
-      <h1>Navbar</h1>
-    </nav>
-  )
+	return (
+		<nav>
+			<h1 className="text-center  font-courgette text-2xl">
+				Intersectionality Club
+			</h1>
+			<ul className="flex justify-center font-inter">
+				<li className="mx-2">
+					<a href="/blog">Home</a>
+				</li>
+				<li className="mx-2">
+					<a href="/about">
+						<a>About Us</a>
+					</a>
+				</li>
+				<li className="mx-2">
+					<a href="/events">
+						<a>Events</a>
+					</a>
+				</li>
+				<li className="mx-2">
+					<a href="/resources">
+						<a>Resources</a>
+					</a>
+				</li>
+			</ul>
+		</nav>
+	);
 }
